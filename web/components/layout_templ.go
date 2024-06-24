1fd79149651b64a0e3f1a59c9538bdba6d15898d
Randy Cahyana <randy@rndir.cc>
2024-06-24 16:09:46 +0700

    fix typo

--- web/components/layout_templ.go
+++ web/components/layout_templ.go
@@ -67,7 +67,7 @@ func footer(name, storePath string) templ.Component {
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(storePath)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/components/layout.templ`, Line: 33, Col: 84}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/components/layout.templ`, Line: 32, Col: 78}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
